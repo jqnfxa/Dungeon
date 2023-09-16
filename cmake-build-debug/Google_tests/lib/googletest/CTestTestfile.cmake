a20838026d65348da8fc815cc9a25c16d43800e8
BasedOnCpp <ivan.boriso2014@gmail.com>
2023-09-16 17:53:22 +0300

    lb2(google_tests)

---
+++ cmake-build-debug/Google_tests/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/shard/CLionProjects/DungeonGame/Google_tests/lib/googletest
+# Build directory: /home/shard/CLionProjects/DungeonGame/cmake-build-debug/Google_tests/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
